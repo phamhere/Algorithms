0f1814b7219a001cc97475ac1e10f3bc11d3971a
Peter Pham <phamhere@gmail.com>
2019-02-20 10:32:14 -0700

    implemented stock_prices algorithm

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,7 +4,18 @@ import argparse
 
 
 def find_max_profit(prices):
-    pass
+    # setting a starting max_profit to compare against
+    max_profit = prices[1] - prices[0]
+    # looping through prices
+    for i in range(len(prices)):
+        """looping through prices for each price, starting at i to not
+        going backwards since need to buy first"""
+        for j in range(i, len(prices) - 1):
+            """comparing the profit of j+1 since don't want to compare
+            same values incase profit is negative"""
+            if prices[j + 1] - prices[i] > max_profit:
+                max_profit = prices[j + 1] - prices[i]
+    return max_profit
 
 
 if __name__ == '__main__':
