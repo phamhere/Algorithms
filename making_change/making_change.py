81a5de3ee464f473b904eb6d7d1ae3a3cbdc4f02
Peter Pham <phamhere@gmail.com>
2019-02-21 15:54:49 -0700

    implemented making_change with Brian Ruff's help

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -4,33 +4,31 @@ import sys
 
 
 def making_change(amount, denominations):
-    # base cases
-    if amount < 5:
-        return 1
-    elif amount < 10:
-        return 2
-    elif amount < 15:
-        return 4
-    elif amount < 20:
-        return 6
-    elif amount < 25:
-        return 9
-    elif amount < 30:
-        return 13
-    elif amount < 35:
-        return 18
+    # making cache to store
+    cache = [0] * (amount + 1)
+    # for giving pennies a starting value
+    cache[0] = 1
 
-    return making_change()
+    # looping through numbers in denominations
+    for denom in denominations:
+        # looping from starting at the denom value up to amount value
+        for upper in range(denom, amount+1):
+            inner_index = upper - denom
+            # getting values from previous caches
+            cache[upper] += cache[inner_index]
 
+    return cache[amount]
 
+# the amount change with each 5, every 10 it increases by an extra, every coin it increases by an extra
 # n(0) = 1
 # n(1) = 1
 # n(5) = 2
 # n(10) = 4
-# n(15) = 6
+# n(15) = [1*15][1*10,5][1*5,5*2][5*3][1*5,10][5,10] so 6
 # n(20) = [1*20][1*15,5][1*10,5*2][1*5,5*3][5*4][1*10,10][10*2][5*2,10][1*5,5,10] so 9
 # n(25) = [[1*25],[1*20,5],[1*15,5*2],[1*10,5*3],[1*5,5*4],[5*5],[1*15,10],[1*5,10*2],[25],[5*3,10],[5,10*2],[1*10,5,10],[1*5,5*2,10]] so 13
 # n(30) = 18
+
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
